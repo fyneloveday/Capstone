d6e8923df2cf6fae839c856eec4f5842e6732a19
fyneloveday <jbfyne18@yahoo.com>
2019-03-22 16:07:18 -0500

    Added a MemberModel class

---
+++ Capstone/Capstone/Models/MemberModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class MemberModel
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
