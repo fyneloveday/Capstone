26487c4ad3ce500e5ffab0a752894ad85f4827e1
fyneloveday <jbfyne18@yahoo.com>
2019-03-24 19:08:29 -0500

    Deleted MemberModelController and MemberModelView classess, andadded MemberController and MemberView classes

---
+++ Capstone/Capstone/Controllers/MemberController.cs
@@ -0,0 +1,94 @@
+﻿using Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Controllers
+{
+    public class MemberController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext;
+
+        // GET: Member
+        public ActionResult Index()
+        {
+            var members = db.MemberModels.ToList();
+            return View(members);
+        }
+
+        // GET: Member/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Member/Create
+        public ActionResult Create()
+        {
+            var members = db.MemberModels.
+            return View();
+        }
+
+        // POST: Member/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Member/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Member/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Member/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Member/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
