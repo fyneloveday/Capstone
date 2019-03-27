67a2dde807dfc72183bb4ea076ef1799610ffa8d
fyneloveday <jbfyne18@yahoo.com>
2019-03-27 10:58:56 -0500

    Added necessary properties to BookAPIModel

--- Capstone/Capstone/Models/BookAPIModel.cs
+++ Capstone/Capstone/Models/BookAPIModel.cs
@@ -7,5 +7,10 @@ namespace Capstone.Models
 {
     public class BookAPIModel
     {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int ISBN { get; set; }
+        public string Synopsis { get; set; }
     }
 }
