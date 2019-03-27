1197ec030daf4cbf02866a9f32e310041a319d4b
fyneloveday <jbfyne18@yahoo.com>
2019-03-27 08:50:24 -0500

    Created a BookEntryModel that allows members to submit a book for consideration in the offchance it's not already in any conventional listings

---
+++ Capstone/Capstone/Models/BookEntryModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class BookEntryModel
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "Author First Name")]
+        public string AuthorFirstName { get; set; }
+        [Display(Name = "Author Middle Name")]
+        public string AuthorMiddleName { get; set; }
+        [Required]
+        [Display(Name = "Author Last Name")]
+        public string AuthorLastName { get; set; }
+        public int YearPublished { get; set; }
+        public int ISBN { get; set; }
+        [Required]
+        [Display(Name = "Publisher")]
+        public string Publisher { get; set; }
+        [Display(Name = "Synopsis")]
+        public string Synopsis { get; set; }
+
+    }
+}
