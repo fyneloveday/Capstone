21049d6365489cd6f7699c052ba0233650a7c6f7
fyneloveday <jbfyne18@yahoo.com>
2019-03-28 12:11:57 -0500

    Added MemberModel Foreign Key to GroupModel

--- Capstone/Capstone/Models/GroupModel.cs
+++ Capstone/Capstone/Models/GroupModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -16,7 +17,9 @@ namespace Capstone.Models
         [Display(Name = "Description")]
         public string Description { get; set; }
         [Display(Name = "Rules")]
-        public string Rules { get; set; }
-
+        public string Rules { get; set; } 
+        [ForeignKey("MemberModel")]
+        public int? MemberModelId { get; set; }
+        public MemberModel MemberModel { get; set; }
     }
 }
