60121a242a1669ab7a1f6912ccda0a37201bd3c9
fyneloveday <jbfyne18@yahoo.com>
2019-03-29 10:10:33 -0500

    Created GroupAdminController with GroupAdmin View

---
+++ Capstone/Capstone/Models/GroupAdminController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Models
+{
+    public class GroupAdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: GroupAdmin
+        public ActionResult Index()
+        {
+            var groupModels = db.GroupModels.Include(g => g.MemberModel);
+            return View(groupModels.ToList());
+        }
+
+        // GET: GroupAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupModel groupModel = db.GroupModels.Find(id);
+            if (groupModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groupModel);
+        }
+
+        // GET: GroupAdmin/Create
+        public ActionResult Create()
+        {
+            ViewBag.MemberModelId = new SelectList(db.MemberModels, "ID", "FirstName");
+            return View();
+        }
+
+        // POST: GroupAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,GroupName,Description,Rules,MemberModelId")] GroupModel groupModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GroupModels.Add(groupModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MemberModelId = new SelectList(db.MemberModels, "ID", "FirstName", groupModel.MemberModelId);
+            return View(groupModel);
+        }
+
+        // GET: GroupAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupModel groupModel = db.GroupModels.Find(id);
+            if (groupModel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MemberModelId = new SelectList(db.MemberModels, "ID", "FirstName", groupModel.MemberModelId);
+            return View(groupModel);
+        }
+
+        // POST: GroupAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,GroupName,Description,Rules,MemberModelId")] GroupModel groupModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(groupModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MemberModelId = new SelectList(db.MemberModels, "ID", "FirstName", groupModel.MemberModelId);
+            return View(groupModel);
+        }
+
+        // GET: GroupAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GroupModel groupModel = db.GroupModels.Find(id);
+            if (groupModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(groupModel);
+        }
+
+        // POST: GroupAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GroupModel groupModel = db.GroupModels.Find(id);
+            db.GroupModels.Remove(groupModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
